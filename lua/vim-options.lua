0b3569450f0e8ba184c5cdde98f27898c080bbdf
Tony McCallie <tonymccallie@gmail.com>
2024-01-16 15:49:25 -0700

    some options in vim-options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set smartindent")
+vim.cmd("set incsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set hlsearch")
+vim.cmd("set relativenumber")
+
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
@@ -13,7 +19,7 @@ vim.keymap.set('n', '<c-t>', ':tab new<CR>')
 vim.keymap.set('n', '<c-.>', ':tabn<CR>')
 vim.keymap.set('n', '<c-,>', ':tabp<CR>')
 
-
+-- Indentation
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 
